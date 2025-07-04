cb9f3935fe60767b852e26785d7c4b2a48797340
andre <andreqt8@gmail.com>
2025-07-04 10:36:08 +0100

    initial commit

---
+++ BinarySearchTrees.js
@@ -0,0 +1,28 @@
+// A BST allows fast operations for lookup, insertion, and deletion of data items.
+
+function Node () {
+
+}
+
+function Tree() {
+    let root = null;
+
+    const buildTree = () => {
+
+    }
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+//console.log(prettyPrint());
