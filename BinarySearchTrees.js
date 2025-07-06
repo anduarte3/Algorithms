2ea573861aff7e9e931e6875fc8b69815f9d6b7d
andre <andreqt8@gmail.com>
2025-07-06 19:23:58 +0100

    PrettyPrint helpful function working, and implementing buildtree function currently

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -1,16 +1,38 @@
 // A BST allows fast operations for lookup, insertion, and deletion of data items.
 
-function Node () {
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
+function Node (value) {
+    let node = { 
+        value: value, 
+        left: null, 
+        right: null
+    };
+
+    return node;
 }
 
-function Tree() {
+function Tree(arr) {
     let root = null;
-
+    
     const buildTree = () => {
+        let start = 0;
+        let end = arr.length-1;
+        let mid = Math.round((start + end) / 2);
 
+        // console.log("Start:", start);
+        // console.log("End:", end);
+        // console.log("Mid:", mid);
+        
+        root = Node(arr[mid]);        
+        console.log(root);
+        
+        return root;
     }
+
+    return { root, buildTree }
 }
+
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node === null) {
@@ -25,4 +47,7 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   }
 };
 
-//console.log(prettyPrint());
+
+const tree = Tree(arr);
+console.log("Tree Node:", tree.buildTree());
+console.log(prettyPrint(tree.buildTree()));
